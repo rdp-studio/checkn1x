cadfce74e7cbc42290da6e54e0bf861f3a780075
rdp-studio <admin@rdpstudio.top>
2021-05-15 22:36:52 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,28 @@
+![](https://github.com/rdp-studio/checkn1x/blob/master/icon_dark.png?raw=true)
+
+# checkn1x
+
+Linux-based distribution for jailbreaking iOS devices w/ checkra1n.
+
+## Downloads
+
+Downloads are available under [releases](https://github.com/rdp-studio/checkn1x/releases).
+
+## Usage
+
+**Use whatever tool you want, but I'm only officially supporting Etcher.**
+
+1. Download [Etcher](https://etcher.io) and the ISO from releases.
+2. Open the ``.iso`` you downloaded in Etcher.
+3. Write it to your USB drive.
+4. Reboot and enter your BIOS's boot menu.
+5. Select the USB drive.
+
+## Building
+
+* The ``CRSOURCE`` variable is the direct link to the build of checkra1n that will be used.
+* Add something to the ``VERSION`` variable if you want to redistribute your image, i.e. ``1.0.6-foo``.
+
+```sh
+docker run -it -v $(pwd):/app --rm --privileged ubuntu "cd /app && /app/build.sh"
+```
---
+++ build.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#
+# checkn1x build script
+# https://asineth.gq/checkn1x
+#
+VERSION="1.0.0"
+ROOTFS="https://dl-cdn.alpinelinux.org/alpine/v3.13/releases/x86_64/alpine-minirootfs-3.13.5-x86_64.tar.gz"
+CRBINARY="https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n"
+
+apt install -y curl ca-certificates tar gzip grub2-common grub-pc-bin grub-efi-amd64-bin xorriso mtools
+
+# clean up previous attempts
+umount -v work/rootfs/dev >/dev/null 2>&1
+umount -v work/rootfs/sys >/dev/null 2>&1
+umount -v work/rootfs/proc >/dev/null 2>&1
+rm -rf work
+mkdir -pv work/{rootfs,iso/boot/grub}
+cd work
+
+# fetch rootfs
+curl -sL "$ROOTFS" | tar -xzC rootfs
+mount -vo bind /dev rootfs/dev
+mount -vt sysfs sysfs rootfs/sys
+mount -vt proc proc rootfs/proc
+cp /etc/resolv.conf rootfs/etc
+cat << ! > rootfs/etc/apk/repositories
+http://dl-cdn.alpinelinux.org/alpine/v3.12/main
+http://dl-cdn.alpinelinux.org/alpine/edge/community
+http://dl-cdn.alpinelinux.org/alpine/edge/testing
+!
+
+# rootfs packages & services
+cat << ! | chroot rootfs /usr/bin/env PATH=/usr/bin:/bin:/usr/sbin:/sbin /bin/sh
+apk upgrade
+apk add alpine-base ncurses-terminfo-base udev usbmuxd libusbmuxd-progs openssh-client sshpass usbutils
+apk add --no-scripts linux-lts linux-firmware-none
+rc-update add bootmisc
+rc-update add hwdrivers
+rc-update add udev
+rc-update add udev-trigger
+rc-update add udev-settle
+!
+
+# kernel modules
+cat << ! > rootfs/etc/mkinitfs/features.d/checkn1x.modules
+kernel/drivers/usb/host
+kernel/drivers/hid/usbhid
+kernel/drivers/hid/hid-generic.ko
+kernel/drivers/hid/hid-cherry.ko
+kernel/drivers/hid/hid-apple.ko
+kernel/net/ipv4
+!
+chroot rootfs /usr/bin/env PATH=/usr/bin:/bin:/usr/sbin:/sbin \
+	/sbin/mkinitfs -F "checkn1x" -k -t /tmp -q $(ls rootfs/lib/modules)
+rm -rfv rootfs/lib/modules
+mv -v rootfs/tmp/lib/modules rootfs/lib
+find rootfs/lib/modules/* -type f -name "*.ko" | xargs -n1 -P`nproc` -- strip -v --strip-unneeded
+find rootfs/lib/modules/* -type f -name "*.ko" | xargs -n1 -P`nproc` -- xz --x86 -v9eT0
+depmod -b rootfs $(ls rootfs/lib/modules)
+
+# unmount fs
+umount -v rootfs/dev
+umount -v rootfs/sys
+umount -v rootfs/proc
+
+# fetch resources
+curl -Lo rootfs/usr/local/bin/checkra1n "$CRBINARY"
+
+# copy files
+cp -av ../inittab rootfs/etc
+cp -av ../scripts/* rootfs/usr/local/bin
+chmod -v 755 rootfs/usr/local/bin/*
+ln -sv sbin/init rootfs/init
+ln -sv ../../etc/terminfo rootfs/usr/share/terminfo # fix ncurses
+
+# boot config
+cp -av rootfs/boot/vmlinuz-lts iso/boot/vmlinuz
+cat << ! > iso/boot/grub/grub.cfg
+insmod all_video
+echo 'checkn1x $VERSION : https://github.com/rdp-studio/checkn1x'
+linux /boot/vmlinuz quiet loglevel=3
+initrd /boot/initramfs.xz
+boot
+!
+
+# initramfs
+pushd rootfs
+rm -rfv tmp/*
+rm -rfv boot/*
+rm -rfv var/cache/*
+rm -fv etc/resolv.conf
+find . | cpio -oH newc | xz -C crc32 --x86 -vz9eT0 > ../iso/boot/initramfs.xz
+popd
+
+# iso creation
+grub-mkrescue -o "checkn1x-$VERSION.iso" iso --compress=xz

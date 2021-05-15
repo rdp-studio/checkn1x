a850628b90f2c859c0dd841ecd87e0fa0ce7674c
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2021-05-16 01:53:51 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ VERSION="1.0.1"
 ROOTFS="https://dl-cdn.alpinelinux.org/alpine/v3.13/releases/x86_64/alpine-minirootfs-3.13.5-x86_64.tar.gz"
 CRBINARY="https://assets.checkra.in/downloads/linux/cli/x86_64/dac9968939ea6e6bfbdedeb41d7e2579c4711dc2c5083f91dced66ca397dc51d/checkra1n"
 
-apt update
-apt install -y curl ca-certificates tar gzip grub2-common grub-pc-bin grub-efi-amd64-bin xorriso mtools xz-utils cpio
+sudo apt update
+sudo apt install -y curl ca-certificates tar gzip grub2-common grub-pc-bin grub-efi-amd64-bin xorriso mtools xz-utils cpio
 
 # clean up previous attempts
 umount -v work/rootfs/dev >/dev/null 2>&1
